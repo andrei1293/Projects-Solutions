9a17e611af7f90a9277c9b8d19099bb430c5f2c0
Andrii Kopp <kopp93@gmail.com>
2017-10-01 12:03:32 +0300

    Refactor service.

--- tax-calculator/src/service/TaxCalculatorService.java
+++ tax-calculator/src/service/TaxCalculatorService.java
@@ -19,18 +19,31 @@ public class TaxCalculatorService extends HttpServlet {
 	private static final String COST = "cost";
 	private static final String TAX = "tax";
 	private static final String TOTAL = "total";
+	private static final String ERROR = "error";
+
+	private static final String POSITIVE_MESSAGE = "Cost and tax values should be positive!";
+	private static final String VALUES_MESSAGE = "Enter cost and tax values!";
 
 	@GET
 	@Path("/calculator")
 	@Produces(MediaType.APPLICATION_JSON)
-	public String test(@QueryParam(COST) String cost, @QueryParam(TAX) String tax) throws JSONException {
-		BigDecimal costValue = new BigDecimal(cost);
-		BigDecimal taxValue = new BigDecimal(tax);
-
-		BigDecimal totalValue = costValue.add(costValue.multiply(taxValue));
-
+	public String calculateTotalCost(@QueryParam(COST) String cost, @QueryParam(TAX) String tax) throws JSONException {
 		JSONObject result = new JSONObject();
-		result.put(TOTAL, totalValue);
+
+		if (cost != null && tax != null && !cost.isEmpty() && !tax.isEmpty()) {
+			BigDecimal costValue = new BigDecimal(cost);
+			BigDecimal taxValue = new BigDecimal(tax).divide(new BigDecimal(100));
+
+			if (costValue.signum() == 1 && taxValue.signum() == 1) {
+				BigDecimal totalValue = costValue.add(costValue.multiply(taxValue));
+
+				result.put(TOTAL, totalValue);
+			} else {
+				result.put(ERROR, POSITIVE_MESSAGE);
+			}
+		} else {
+			result.put(ERROR, VALUES_MESSAGE);
+		}
 
 		return result.toString();
 	}
